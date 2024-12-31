393a0fc0d124062ee1eb442c1232d12e571f1a76
naeemudheen p <nayeemudheenaslam3@gmail.com>
2024-12-31 18:08:08 +0530

    Added support for rhc

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-=======
-
->>>>>>> 407cbeea660bde6d6b22663deed85cf1394610ab
